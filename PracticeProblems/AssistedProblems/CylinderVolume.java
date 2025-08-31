d2134c51795fa39c67e5251bb525cde1f63d43f6
Mahi-agrawal84 <agrawalmahi18012006@gmail.com>
2025-08-31 12:48:33 +0530

    Add files via upload

---
+++ PracticeProblems/AssistedProblems/CylinderVolume.java
@@ -0,0 +1,12 @@
+package AssistedProblems;
+import java.util.Scanner;
+public class CylinderVolume {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double radius = sc.nextDouble();
+        double height = sc.nextDouble();
+        double volume = Math.PI * radius * radius * height;
+        System.out.println("Volume: " + volume);
+    }
+}
+
