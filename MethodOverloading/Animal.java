ff1db7756d16dddf56eb915515a09adf69098cac
Mahi-agrawal84 <agrawalmahi18012006@gmail.com>
2025-09-12 20:42:33 +0530

    Add files via upload

---
+++ MethodOverloading/Animal.java
@@ -0,0 +1,22 @@
+package MethodOverloading;
+public class Animal {
+    public void speak() {
+        System.out.println("Animal makes a sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    public void speak() {
+        System.out.println("Cat meows");
+    }
+}
+
+
